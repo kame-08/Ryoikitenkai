11ec09bcb5a1bf3fc3c87d5cda4c31be6ee15749
ryo <21cm0108@jec.ac.jp>
2022-11-23 11:06:10 +0900

    Initial Commit

---
+++ Ryoikitenkai/RyoikitenkaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RyoikitenkaiApp.swift
+//  Ryoikitenkai
+//
+//  Created by Ryo on 2022/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RyoikitenkaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
